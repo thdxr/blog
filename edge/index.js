34bf750723665764f4b162e3544148ee77d21fee
Dax Ravi <d@ironbay.co>
2021-10-10 20:46:29 -0400

    Redirects

--- edge/index.js
+++ edge/index.js
@@ -1,5 +1,18 @@
 function handler(event) {
-  if (event.request.uri !== "/" && !event.request.uri.includes("."))
-    event.request.uri += ".html";
+  var uri = event.request.uri;
+  if (uri.startsWith("/about")) return redir("/");
+  if (uri.startsWith("/followers")) return redir("/");
+
+  if (uri !== "/" && !uri.includes(".")) event.request.uri += ".html";
   return event.request;
+}
+
+function redir(location) {
+  return {
+    statusCode: 302,
+    statusDescription: "Found",
+    headers: {
+      location: { value: location },
+    },
+  };
 }
